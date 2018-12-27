706d388b7ff9459d445e208eec7c37caeba341b7
Alain Galvan <hi@alain.xyz>
2018-12-26 19:30:36 -0500

    Fixed type definitions

--- cross-shader.d.ts
+++ cross-shader.d.ts
@@ -1,40 +1,39 @@
 declare enum ShaderFormat {
-    GLSL,
-    HLSL,
-    MSL,
-    SPIRV,
-    ShaderFormatMax
+	GLSL,
+	HLSL,
+	MSL,
+	SPIRV,
+	ShaderFormatMax
 }
 
 declare enum ShaderStage {
-    Vertex,
-    TessControl,
-    TessEvaluation,
-    Geometry,
-    Fragment,
-    Compute
+	Vertex,
+	TessControl,
+	TessEvaluation,
+	Geometry,
+	Fragment,
+	Compute
 }
 
-
 type InputOptions = {
-  // All
-  format: ShaderFormat,
-  stage: ShaderStage,
+	// All
+	format: ShaderFormat;
+	stage: ShaderStage;
 
-  // ⚪ GLSL
-  es?: boolean,
-  glslVersion?: number
-
+	// ⚪ GLSL
+	es?: boolean;
+	glslVersion?: number;
 };
 
 type OutputOptions = {
-  // All
-  format: ShaderFormat,
+	// All
+	format: ShaderFormat;
 
-  // ⚪ GLSL
-  es?: boolean,
-  glslVersion?: number
-
+	// ⚪ GLSL
+	es?: boolean;
+	glslVersion?: number;
 };
 
 type compile = (inputString: string, ioptions: InputOptions, ooptions: OutputOptions) => string;
+
+export default Promise<{ compile: compile; ShaderFormat: ShaderFormat; ShaderStage: ShaderStage }>();
