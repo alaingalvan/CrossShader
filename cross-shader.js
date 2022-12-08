656fc0abb657c03bdfeb6ded0ab9fa57da2a9f97
romamik <roman@romamik.com>
2022-12-08 18:32:47 +0200

    Update cross-shader.js

--- cross-shader.js
+++ cross-shader.js
@@ -2,15 +2,13 @@ let xsdrModule = require('./bin/CrossShader.js');
 
 const m = xsdrModule();
 
-module.exports = {
-    default: new Promise((res, rej) => {
-        m.onRuntimeInitialized = () => {
-            res({
-                ShaderFormat: m.ShaderFormat,
-                ShaderStage: m.ShaderStage,
-                compile: m.compile,
-                default: m.compile
-            });
-        };
-    })
-};
+module.exports = new Promise((res, rej) => {
+	m.onRuntimeInitialized = () => {
+		res({
+			ShaderFormat: m.ShaderFormat,
+			ShaderStage: m.ShaderStage,
+			compile: m.compile,
+			default: m.compile
+		});
+	};
+})
